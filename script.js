14e30b5db0d628524c7b0d1ff093720579288451
robert-devs <mainrobert04@gmail.com>
2022-07-27 22:52:23 +0300

    updates

--- script.js
+++ script.js
@@ -1,33 +1,83 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var todos = [];
+var edited;
+var isEditedTodo = false;
 var todosContainer = document.getElementById("mytask");
 var titleInput = document.getElementById("title-input");
 var dateInput = document.getElementById("date-input");
 var descriptionInput = document.getElementById("description-input");
-var addTodo = function (title, description, date) {
-    var id = Math.ceil(Math.random() * 100000000);
-    var todo = { id: id, title: title, description: description, date: date, completed: false };
-    var newTodos = __spreadArray(__spreadArray([], todos, true), [todo], false);
-    loadTodos(newTodos);
-    todos.push(todo);
-};
-var loadTodos = function (todos) {
-    if (todos.length > 0) {
-        var todosHtml = todos.map(function (id, date, title, description, completed) {
-            var element = "<li class=\"task-item ".concat(completed ? "completed" : "", "\" id=\"").concat(id, "\">\n                <input type=\"checkbox\" ").concat(completed ? "checked" : "", " onchange=\"checkBoxChange(this)\" /> \n                <div class=\"task-body\">\n                    <h4>").concat(title, " <span class=\"due-date\">Due: ").concat(date, "</span></h4>\n                    <p>").concat(description, "<span class=\"status\">").concat(completed ? " completed" : "pending", "</span></p>\n                </div>\n                <span class=\"close\" onclick=\"deleteTodo(this)\">\u00D7</span>\n                </li>");
-            return element;
-        });
-        todosContainer.innerHTML = todosHtml.join("");
+var Tasks = /** @class */ (function () {
+    function Tasks() {
+        var _this = this;
+        this.todosArray = [];
+        this.loadTodos = function () {
+            if (_this.todosArray.length > 0) {
+                var todosHtml = _this.todosArray.map(function (_a) {
+                    var id = _a.id, date = _a.date, title = _a.title, description = _a.description, completed = _a.completed;
+                    var element = "<li class=\"task-item ".concat(completed ? "completed" : "", "\" id=\"").concat(id, "\">\n                <input type=\"checkbox\" ").concat(completed ? "checked" : "", " onchange=\"checkBoxChange(this)\" /> \n                <div class=\"task-body\">\n                    <h4>").concat(title, " <span class=\"due-date\">Due: ").concat(date, "</span></h4>\n                    <p>").concat(description, "<span class=\"status\">").concat(completed ? " completed" : "pending", "</span></p>\n                </div>\n                <span class=\"edit\" onclick=\"editTodo(this)>edit</span>\n                <span class=\"close\" onclick=\"deleteTodo(this)\">\u00D7</span>\n                </li>");
+                    return element;
+                });
+                todosContainer.innerHTML = todosHtml.join("");
+            }
+            else {
+                todosContainer.innerHTML = "<h2 style='text-align: center;'>No Todos Yet</h2>";
+            }
+        };
+        this.deleteTodo = function (id) {
+            // console.log(e);
+            var updatedTodos = _this.todosArray.filter(function (todo) { return todo.id !== id; });
+            _this.todosArray = updatedTodos;
+            _this.loadTodos();
+        };
+        this.editTodo = function (todoId) {
+            console.log(todoId);
+            var editId = todoId;
+            var isEditedTodo = true;
+        };
     }
-    else {
-        todosContainer.innerHTML = "<h2 style='text-align: center;'>No Todos Yet</h2>";
+    Tasks.prototype.addTodo = function (todo) {
+        this.todosArray.push(todo);
+        this.loadTodos();
+    };
+    Tasks.prototype.getTodo = function () {
+        return this.todosArray;
+    };
+    return Tasks;
+}());
+var tasks = new Tasks();
+var newTodoSubmit = function (e) {
+    var titleValue = titleInput.value;
+    var dateValue = dateInput.value;
+    var descriptionValue = descriptionInput.value;
+    if (!titleValue) {
+        alert("Title is required");
+        return;
     }
+    if (!dateValue) {
+        alert("Date is required");
+        return;
+    }
+    if (!descriptionValue) {
+        alert("Dscription is required");
+        return;
+    }
+    var newTodo = {
+        title: titleValue,
+        description: descriptionValue,
+        date: dateValue,
+        completed: false,
+        id: Math.floor(Math.random() * 10000000)
+    };
+    tasks.addTodo(newTodo);
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
 };
+// const checkBoxChange = (e: any) => {
+//     const todoId = e.parentElement.id
+//     const updatedTodos = todos.map((todo: { id: number; completed: true; }) => {
+//         // console.log(todo.id, todoId);
+//         if (todo.id === parseInt(todoId)) todo.completed = e.checked
+//         return todo
+//     })
+//     loadTodos(updatedTodos)
+//     todos = updatedTodos
+// }
--- script.ts
+++ script.ts
@@ -1,61 +1,134 @@
-let todos:any = []
+let edited: string
+let isEditedTodo: boolean = false;
+
 const todosContainer = document.getElementById("mytask");
-const titleInput = document.getElementById("title-input");
-const dateInput = document.getElementById("date-input");
-const descriptionInput = document.getElementById("description-input");
+const titleInput = document.getElementById("title-input") as HTMLInputElement;
+const dateInput = document.getElementById("date-input") as HTMLInputElement;
+const descriptionInput = document.getElementById("description-input") as HTMLInputElement;
 
-const addTodo = (title:string, description:string, date:number) => {
-    const id = Math.ceil(Math.random() * 100000000);
-    const todo = { id, title, description, date, completed: false }
+interface Todo {
+    title: any
+    description: any
+    date: string
+    id: number
+    completed: boolean;
 
-    const newTodos = [...todos, todo]
-    loadTodos(newTodos)
-
-    todos.push(todo)
 }
-const loadTodos = (todos:any) => {
-    if (todos.length > 0) {
-        const todosHtml = todos.map(( id:number, date:number, title:string, description:string, completed:any ) => {
-            const element = `<li class="task-item ${completed ? "completed" : ""}" id="${id}">
+class Tasks {
+
+    private todosArray: Todo[] = []
+    constructor
+        () { }
+    addTodo(todo: Todo) {
+        
+        this.todosArray.push(todo)
+        this.loadTodos()
+    }
+    getTodo() {
+        return this.todosArray;
+    }
+
+    loadTodos = () => {
+    if (this.todosArray.length > 0) {
+        const todosHtml = this.todosArray.map(({ id, date, title, description, completed }) => {
+            const element: any = `<li class="task-item ${completed ? "completed" : ""}" id="${id}">
                 <input type="checkbox" ${completed ? "checked" : ""} onchange="checkBoxChange(this)" /> 
                 <div class="task-body">
                     <h4>${title} <span class="due-date">Due: ${date}</span></h4>
                     <p>${description}<span class="status">${completed ? " completed" : "pending"}</span></p>
+                    <p>${description}<span class="">${completed ? " completed" : "pending"}</span></p>
                 </div>
                 <span class="close" onclick="deleteTodo(this)">\u00D7</span>
                 </li>`
 
-           return element
+            return element
         })
-        todosContainer.innerHTML = todosHtml.join("")
+        todosContainer!.innerHTML = todosHtml.join("")
     } else {
-        todosContainer.innerHTML = "<h2 style='text-align: center;'>No Todos Yet</h2>"
+        todosContainer!.innerHTML = "<h2 style='text-align: center;'>No Todos Yet</h2>"
     }
 }
 
-class newTodoSubmit  () => {
-    const titleValue = titleInput.value:String;
+deleteTodo = (id: number) => {
+    // console.log(e);
+    const updatedTodos = this.todosArray.filter((todo: { id: number; }) => todo.id !== id)
+
+    this.todosArray = updatedTodos
+    this.loadTodos()
+}
+ editTodo = (todoId: Number,) =>{
+    console.log(todoId,)
+    const editId = todoId;
+    const isEditedTodo = true;
+
+}
+ checkBoxChange = (e:any) => {
+    const todoId = e.parentElement.id
+
+    const updatedTodos = this.todosArray.map(todo => {
+        // console.log(todo.id, todoId);
+        if (todo.id === parseInt(todoId)) todo.completed = e.checked
+        return todo
+    })
+    this.todosArray =updatedTodos
+    this.todosArray = updatedTodos
+}
+
+}
+
+const tasks = new Tasks()
+
+const newTodoSubmit = (e: any) => {
+    const titleValue = titleInput.value;
     const dateValue = dateInput.value;
     const descriptionValue = descriptionInput.value;
+
+
 
     if (!titleValue) {
         alert("Title is required")
         return
     }
 
-//     if (!dateValue) {
-//         alert("Date is required")
-//         return
-//     }
+    if (!dateValue) {
+        alert("Date is required")
+        return
+    }
 
-//     if (!descriptionValue) {
-//         alert("Dscription is required")
-//         return
-//     }
+    if (!descriptionValue) {
+        alert("Dscription is required")
+        return
+    }
 
-//     addTodo(titleValue, descriptionValue, dateValue)
-//     titleInput.value = ""
-//     dateInput.value = ""
-//     descriptionInput.value = ""
+    const newTodo:Todo = {
+        title: titleValue,
+        description: descriptionValue,
+        date: dateValue,
+        completed: false,
+        id: Math.floor(Math.random() * 10000000)
+    }
+
+    tasks.addTodo(newTodo)
+
+    titleInput.value = ""
+    dateInput.value = ""
+    descriptionInput.value = ""
+
+
+}
+
+// const checkBoxChange = (e: any) => {
+//     const todoId = e.parentElement.id
+
+//     const updatedTodos = todos.map((todo: { id: number; completed: true; }) => {
+//         // console.log(todo.id, todoId);
+//         if (todo.id === parseInt(todoId)) todo.completed = e.checked
+//         return todo
+//     })
+//     loadTodos(updatedTodos)
+//     todos = updatedTodos
 // }
-loadTodos(todos)
+
+
+
+
